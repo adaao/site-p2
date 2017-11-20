4d00c040bfcb740ac683a6910e0728e63d913075
adaao <armascarelli@gmail.com>
2017-11-20 11:56:49 -0200

    implementacao da tabela de tipo de usuarios

--- haskell-p2/src/Handler/Home.hs
+++ haskell-p2/src/Handler/Home.hs
@@ -8,3 +8,6 @@ module Handler.Home where
 import Import
 import Yesod.Form.Bootstrap3 (BootstrapFormLayout (..), renderBootstrap3)
 import Text.Julius (RawJS (..))
+
+getHomeR :: Handler Value
+getHomeR = undefined
--- haskell-p2/src/Handler/UserCategory.hs
+++ haskell-p2/src/Handler/UserCategory.hs
@@ -8,3 +8,21 @@ module Handler.UserCategory where
 import Import
 import Network.HTTP.Types.Status
 import Database.Persist.Postgresql
+
+getListUserTypeR :: Handler Value
+getListUserTypeR = undefined
+{-getListUserType = do
+  userTypeList <- runDB $ selectList [userType UserCategory] []
+  sendStatusJSON ok200 (object ["resp" .= (toJSON  userTypeList)])
+-}
+postInsertUserTypeR :: Handler Value
+postInsertUserTypeR = do
+  userType <- requireJsonBody :: Handler UserCategory
+  userCategoryId <- runDB $ insert userType
+  sendStatusJSON created201 (object ["resp" .= (fromSqlKey userCategoryId)])
+
+putUpdateUserTypeR :: UserCategoryId -> Text -> Handler Value
+putUpdateUserTypeR userCategoryId updatedUserType = do
+  _ <- runDB $ get404 userCategoryId
+  runDB $ update userCategoryId [UserCategoryUserType =. updatedUserType]
+  sendStatusJSON noContent204 (object ["resp" .= (fromSqlKey userCategoryId)])
