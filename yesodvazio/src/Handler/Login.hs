eccdd1a96f5b5cac54f09cec74fe36e7126640bb
romefeller <alberto.nunes@primesolutions.com.br>
2017-12-04 01:34:33 +0000

    formulario de login acetado, falta testar

--- yesodvazio/src/Handler/Login.hs
+++ yesodvazio/src/Handler/Login.hs
@@ -31,6 +31,28 @@ getLogarR = do
 autentica :: Text -> Text -> HandlerT App IO (Maybe (Entity User))
 autentica email senha = runDB $ selectFirst [UserEmail ==. email
                                             ,UserPassword ==. senha] []
+                                            
+postLogarR :: Handler Html
+postLogarR = do 
+    ((resultado,_),_) <- runFormPost formLogin
+    case resultado of
+        FormSuccess ("root@root.com","root") -> do 
+            setSession "_NOME" "admin"
+            redirect UserR
+        FormSuccess (email,password) -> do 
+            talvezUser <- autentica email password
+            case talvezUser of 
+                Nothing -> do 
+                    setMessage [shamlet|
+                        <div> 
+                            Usuario nao encontrado/Senha invalida!
+                    |]
+                    redirect LogarR
+                Just (Entity _ usr) -> do 
+                    setSession "_NOME" (userNickName usr)
+                    redirect HomeR
+            redirect UserR
+        _ -> redirect HomeR
 
 postLogSairR :: Handler Html
 postLogSairR = do 
@@ -45,8 +67,8 @@ postLogSairR = do
                             Usuario nao encontrado/Senha invalida!
                     |]
                     redirect HomeR
-                Just (Entity _ cli) -> do 
-                    setSession "_NOME" (userNickName cli)
+                Just (Entity _ usr) -> do 
+                    setSession "_NOME" (userNickName usr)
                     redirect HomeR
             redirect HomeR
         _ -> redirect HomeR
