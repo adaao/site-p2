0c9046e0e46406fb892a0147b0345f545052e88c
romefeller <alberto.nunes@primesolutions.com.br>
2017-12-07 01:14:32 +0000

    organização do User.hs

--- yesodvazio/src/Handler/Users.hs
+++ yesodvazio/src/Handler/Users.hs
@@ -4,7 +4,7 @@
 {-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeFamilies #-}
--- | Common handler functions.
+
 module Handler.Users where
 
 import Import
@@ -13,43 +13,11 @@ import Database.Persist.Postgresql
 formUser :: Form Users
 formUser = undefined 
 
-{-renderDivs $ User
-    <$> areq textField "nickName: " Nothing
-    <*> areq emailField "Email: " Nothing
-    <*> areq passwordField "Senha: " Nothing
-    -- a categoria do usuario deve ser enviada de forma automatica
-    -- não e o suario que escolhe
-   -- <*> areq keyField "Tipo do usuario: " Nothing
--}
-
 getUserR :: Handler Html
 getUserR = undefined 
-{-do 
-    (widget,enctype) <- generateFormPost formUser
-    mensa <- getMessage
-    defaultLayout $ do 
-        [whamlet|
-            $maybe msg <- mensa
-                ^{msg}
-            <form action=@{HomeR} method=post>
-                ^{widget}
-                <input type="submit" value="Cadastrar">
-        |]
--}
+
 postUserR :: Handler Html
 postUserR = undefined
-{-do 
-    ((resultado,_),_) <- runFormPost formUser
-    case resultado of 
-        FormSuccess usr -> do 
-            _ <- runDB $ insert usr
-            setMessage [shamlet|
-                <div> 
-                    Cadastro realizado com sucess!
-            |]
-            redirect UserR
-        _ -> redirect HomeR
--}
 
 postCreateUserR :: Handler Value
 postCreateUserR = do
