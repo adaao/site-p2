9437b2817c1051f44a8ae12dcc2d51c5ddc5f4e2
romefeller <alberto.nunes@primesolutions.com.br>
2017-12-07 01:15:48 +0000

    organização do Reply.hs

--- yesodvazio/src/Handler/Reply.hs
+++ yesodvazio/src/Handler/Reply.hs
@@ -15,22 +15,3 @@ postCreateReplyR = do
     newObj <- requireJsonBody :: Handler Reply
     newObjId <- runDB $ insert newObj
     sendStatusJSON created201 (object ["resp" .= (fromSqlKey newObjId)])
-
-{-
-getListReplyR :: PublicationId -> Handler Value
-getListReplyR pid = do
-    publicationList <- runDB $ selectList [PublicationId .== pid] [Asc PublicationTitle]
-    sendStatusJSON ok200 (object ["resp" .= (toJSON publicationList)])
-
-getReadReplyR :: PublicationId -> Handler Value
-getReadReplyR objId = do
-    obj <- runDB $ get404 objId
-    sendStatusJSON ok200 (object ["resp" .= (toJSON obj)])
-
-putUpdateReplyR :: PublicationId -> Handler Value
-putUpdateReplyR objId = do
-    _ <- runDB $ get404 objId
-    updatedObj <- requireJsonBody :: Handler Reply
-    runDB $ replace objId updatedObj
-    sendStatusJSON noContent204 (object ["resp" .= (fromSqlKey objId)])
--}
