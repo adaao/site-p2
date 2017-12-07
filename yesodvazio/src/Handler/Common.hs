fa9295ac18cec3b89d86c0e4a7a74d357cdc8eb6
romefeller <alberto.nunes@primesolutions.com.br>
2017-12-07 11:37:23 +0000

    ./commitar.sh

--- yesodvazio/src/Handler/Common.hs
+++ yesodvazio/src/Handler/Common.hs
@@ -9,6 +9,8 @@ module Handler.Common where
 import Data.FileEmbed (embedFile)
 import Import
 
+
+
 -- These handlers embed files in the executable at compile time to avoid a
 -- runtime dependency, and for efficiency.
 
